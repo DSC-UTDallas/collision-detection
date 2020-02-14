044247127fc04778dfb295bed62ed9b7527aafa5
Kailash Sub <kailash.s.2012@gmail.com>
2020-02-13 20:42:30 -0600

    Init

---
+++ scraper.py
@@ -0,0 +1,48 @@
+import pytube
+from pytube import YouTube
+import youtube_dl
+import os
+import cv2
+print(cv2.__file__)
+
+cwd = os.getcwd()
+
+# https://www.youtube.com/watch?v=NCmlbDaj-uE
+# https://www.youtube.com/watch?v=rVssEL2jhu8
+# https://www.youtube.com/watch?v=tDN-mwNSJc8
+
+# don't use this. pytube doesn't work.
+def dlv() :
+    yt = YouTube("https://www.youtube.com/watch?v=tDN-mwNSJc8")
+    print(yt.streams.all())
+    stream = yt.streams.first()
+    stream.download()
+
+# use this.
+def dlvydl():
+    print(cwd)
+
+    ydl_opts = {
+        'outtmpl': cwd+'\\%(title)s.%(ext)s'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=NCmlbDaj-uE'])
+
+#https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames
+def extract() :
+    vidcap = cv2.cv2.VideoCapture('Hazelwood Traffic Cameras.mp4')
+    success, image = vidcap.read()
+    count = 0
+    modocount = 0
+    print(success)
+    while success:
+        success,image = vidcap.read()
+        if (modocount % 15 == 0):
+            cv2.cv2.imwrite("raw/img%d.jpg" % count, image)     
+            print('Read a new frame: ', success)
+            count += 1
+            modocount += 1
+
+if __name__ == "__main__":
+    dlvydl()
+    extract()
