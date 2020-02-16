12be654fe601b7479561f95349a7d5d3fb9fc684
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-02-16 12:34:05 -0600

    Exif Data Extractor
    Extracts Data and GPS info from images. GPS still needs some work to become more detailed.

---
+++ Server end/Exif_Exctractor/Exif_Exctractor.py
@@ -0,0 +1,14 @@
+import exifread
+import csv 
+f = open("C:/Users/Owner/source/repos/Exif_Exctractor/exif_test.jpg", 'rb')
+tags = exifread.process_file(f)
+#print(tags)
+
+GPS = tags.get('Image GPSInfo')
+dateTime = tags.get('Image DateTime')
+print(dateTime)
+
+with open('Ledger.csv', mode='w') as ledger:
+    ledger = csv.writer(ledger, delimiter =',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    ledger.writerow([dateTime,GPS])
